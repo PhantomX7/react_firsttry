a327793c053b4f34cc02c076ff487b802c032f65
Phantom <kenichixie@gmail.com>
2017-08-17 20:23:07 +0700

    add youtube API dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const App = function(){
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyA3ZqbqHzbaAEGmE6KJI6QikNEaQe_BbnM';
+
+const App = () => {
 	return <div>Hi!</div>;
 }
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
