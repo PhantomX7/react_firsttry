0aa5710015d5e91a1d0a4171d1dc2e0372bf4456
Kenichi <kenichixie@gmail.com>
2017-08-22 18:04:53 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "React First Try",
+  "description": "A sample react app",
+  "repository": "https://github.com/PhantomX7/react_firsttry",
+  "logo": "https://node-js-sample.herokuapp.com/node.png",
+  "keywords": ["node","react","babel","webpack"]
+}
